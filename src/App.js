150933892216eaafc8f1ba0190221e97ec38b88a
Mogomotsi Thateng <mogomotsithateng@gmail.com>
2025-07-01 21:04:16 +0200

    Added the navbar/navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './Home';
 function App() {
   return (
-    <div className="App">
-        <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
