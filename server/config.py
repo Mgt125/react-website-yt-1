0d36280f61b5459319f9a6129666aa32feb7e226
Mogomotsi Thateng <mogomotsithateng@gmail.com>
2025-07-10 11:16:49 +0200

    Removed Flask backend from git tracking

--- server/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///gameroom.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your-secret-key'  # Replace with something strong
